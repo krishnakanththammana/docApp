351e782e8229c03d595ec1b07d97740fbc4b6b3b
krishnakanththammana <krishnakanththammana@gmail.com>
2019-02-05 17:33:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'docApp')))
 
 app.get('/ping', (req, res) => res.send('pong'));
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/docApp/index.html'));
 
 var port = Number(process.env.PORT || 3001);
 app.listen(port, () => console.log('app listening on port: '+port))
